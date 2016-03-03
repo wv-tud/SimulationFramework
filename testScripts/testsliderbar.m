abe6b825b7cf3e7b3c3290da6d0053077f9943c5
wv-tud <w.vlenterie@student.tudelft.nl>
2016-03-03 13:34:07 +0100

    Removed unneccesary folders containing simdata

--- testScripts/testsliderbar.m
+++
@@ -1,4 +0,0 @@
-F = figure(2);
-set(F,'Position',[100 100 500 600]);
-h = uicontrol('style','slider','units','pixel','position',[20 20 450 20],'min',1,'max',200,'sliderstep',[1/199 1/199],'value',1);
-addlistener(h,'ContinuousValueChange',@(hObject, event) uArena{1}.agents{round(h.Value)}.plotVelocityComponents());
