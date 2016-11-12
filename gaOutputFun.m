8ea09a1784d9d3d461612f09a1634fe064b65f94
wv-tud <w.vlenterie@student.tudelft.nl>
2016-11-12 11:43:32 +0100

    Add outut function template

---
+++ gaOutputFun.m
@@ -0,0 +1,10 @@
+ function[state,options,optchanged] = outputfun_ga(options, state, flag)
+    optchanged = false;
+   % DO NOT set options, state, or flag!
+    % Just write code to calculate whatever it is you want here.
+    % If you need to read an option, then read it, don't set it.
+    % Make sure that your options include @outputfun as your output function.
+    %options
+    %state
+    %flag
+ end
